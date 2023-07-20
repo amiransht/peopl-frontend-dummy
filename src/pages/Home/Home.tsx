ebcc4384516b00bdb9617cdbc0c18f0206eb376c
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-07-20 21:28:11 +0700

    feat: add home page and post card

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./HomeStyle.css";
 import CreatePostHome from "../../components/CreatePost/CreatePostHome";
+import PostCard from "../../components/PostCard/PostCard";
 
 const Home = () => {
   document.body.style.backgroundColor = "var(--neutral-10)";
   return (
     <div className="home-layout">
       <CreatePostHome />
+      <div className="posts">
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
     </div>
   );
 };
